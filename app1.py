920677a02858d7360d1d41c7637fbb0e40bf14da
Vikrant Karawande <viku1996@gmail.com>
2020-05-20 20:39:09 +0530

    added ability to read from excel file

--- app1.py
+++ app1.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url = 'http://www.google.com'
+data = pd.read_excel('Workwear brands.xlsx')
+websites = data['Domain'].tolist()
+weblinks = data['product link'].tolist()
+
 dri = webdriver.Chrome('./chromedriver')
-dri.get(url)
-filename = "./parsedData/"+dri.title
-html = BeautifulSoup(dri.page_source,'html5lib')
-with open(filename+".txt","w",encoding="utf-8") as f:
-    f.write(html.prettify())
-dri.save_screenshot(filename+'.png')
+
+for i in range(len(websites)):
+    url = weblinks[i]
+    filename = "./parsedData/"+websites[i]
+    dri.get(url)
+    
+    html = BeautifulSoup(dri.page_source,'html5lib')
+    with open(filename+".txt","w",encoding="utf-8") as f:
+        f.write(html.prettify())
+    dri.save_screenshot(filename+'.png')
 dri.close()
